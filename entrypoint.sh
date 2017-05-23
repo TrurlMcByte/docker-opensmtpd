65d05aa09c9a770efe09c90c4d0932b402fa5747
Trurl McByte <trurl@mcbyte.net>
2017-05-23 13:05:08 +0300

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,6 @@ chmod 0711 /var/spool/smtpd
 chown -R 101.root /var/vmail /var/spool/smtpd/purge /var/spool/smtpd/queue /var/spool/smtpd/corrupt
 chmod 0700 /var/spool/smtpd/purge
 chown -R root.102 /var/spool/smtpd/offline
+chmod 0770 /var/spool/smtpd/offline
 test -f /etc/smtpd/smtpd.conf || cp /smtpd.conf.sample /etc/smtpd/smtpd.conf
 exec "$@"
