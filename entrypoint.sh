1ac65e6526b4e54c1974bab734bfec774cf5308a
Trurl McByte <trurl@mcbyte.net>
2018-09-02 10:32:06 +0300

    entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 mkdir -p /var/spool/smtpd
-chown -R root.root /var/spool/smtpd
-chmod 0711 /var/spool/smtpd
 mkdir -p /var/vmail
 mkdir -p /var/spool/smtpd/purge
 mkdir -p /var/spool/smtpd/queue
 mkdir -p /var/spool/smtpd/corrupt
+mkdir -p /var/spool/smtpd/offline
+chown -R root.root /var/spool/smtpd
+chmod 0711 /var/spool/smtpd
 chown -R 101.root /var/vmail /var/spool/smtpd/purge /var/spool/smtpd/queue /var/spool/smtpd/corrupt
 chmod 0700 /var/spool/smtpd/purge
-mkdir -p /var/spool/smtpd/offline
 chown -R root.102 /var/spool/smtpd/offline
 chmod 0770 /var/spool/smtpd/offline
+chmod 0700 /var/spool/smtpd/queue /var/spool/smtpd/corrupt
 test -f /etc/smtpd/smtpd.conf || cp /smtpd.conf.sample /etc/smtpd/smtpd.conf
 exec "$@"
